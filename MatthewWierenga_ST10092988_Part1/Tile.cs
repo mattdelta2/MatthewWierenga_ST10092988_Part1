29441d5960869c672db12bf80be8bd63a59b8e40
Matthew Wierenga <matt.wierenga@outlook.com>
2022-08-22 14:14:36 +0200

    Added Tile.CS and Enum.CS

---
+++ MatthewWierenga_ST10092988_Part1/Tile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatthewWierenga_ST10092988_Part1
+{
+    public abstract class Tile
+    {
+        protected int x;
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        protected int y;
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+    }
+}
