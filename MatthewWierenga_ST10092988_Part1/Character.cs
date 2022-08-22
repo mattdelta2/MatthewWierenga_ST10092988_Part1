def5ec1e718ba0550b962bf38be8201fad02fabe
Matthew Wierenga <matt.wierenga@outlook.com>
2022-08-22 13:36:33 +0200

    Added Character.CS

---
+++ MatthewWierenga_ST10092988_Part1/Character.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatthewWierenga_ST10092988_Part1
+{
+     class Character
+    {
+        public int CharacterMaxHP;
+        public int CharacterDamage;
+        public int CharacterHP;
+
+        public int GoblinMaxHP;
+        public int GoblinDamage;
+        public int GoblinHP;
+
+
+
+
+
+        public enum Movement
+        {
+            NoMovement,
+            Up,
+            Down,
+            Right,
+            Left,
+
+        }
+        public bool isDead;
+
+        public bool CheckRange;
+    }
+}
