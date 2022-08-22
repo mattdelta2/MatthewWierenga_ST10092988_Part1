8d14f8d48230997380736fc639a988fbfc34b374
Matthew Wierenga <matt.wierenga@outlook.com>
2022-08-22 13:33:25 +0200

    Added labels to the form for map and character stats

--- MatthewWierenga_ST10092988_Part1/Form1.Designer.cs
+++ MatthewWierenga_ST10092988_Part1/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblMap = new System.Windows.Forms.Label();
+            this.lblCharacterStats = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMap
+            // 
+            this.lblMap.AutoSize = true;
+            this.lblMap.Location = new System.Drawing.Point(291, 82);
+            this.lblMap.Name = "lblMap";
+            this.lblMap.Size = new System.Drawing.Size(35, 13);
+            this.lblMap.TabIndex = 0;
+            this.lblMap.Text = "label1";
+            // 
+            // lblCharacterStats
+            // 
+            this.lblCharacterStats.AutoSize = true;
+            this.lblCharacterStats.Location = new System.Drawing.Point(1157, 81);
+            this.lblCharacterStats.Name = "lblCharacterStats";
+            this.lblCharacterStats.Size = new System.Drawing.Size(35, 13);
+            this.lblCharacterStats.TabIndex = 1;
+            this.lblCharacterStats.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1481, 618);
+            this.Controls.Add(this.lblCharacterStats);
+            this.Controls.Add(this.lblMap);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblMap;
+        private System.Windows.Forms.Label lblCharacterStats;
     }
 }
 
